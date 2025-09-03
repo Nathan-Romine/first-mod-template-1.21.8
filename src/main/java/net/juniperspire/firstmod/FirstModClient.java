69aaa5a39d4f452803583263acf6f98fe2739e72
Nathan-Romine <nrr8574@g.rit.edu>
2025-09-03 11:37:59 -0400

    Messed around with signs. Definately not functional yet.

--- src/main/java/net/juniperspire/firstmod/FirstModClient.java
+++ src/main/java/net/juniperspire/firstmod/FirstModClient.java
@@ -1,12 +1,34 @@
 package net.juniperspire.firstmod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.*;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.Direction;
 
 public class FirstModClient implements ClientModInitializer
 {
     @Override
     public void onInitializeClient()
     {
+        UseBlockCallback.EVENT.register((player, world, hand, hitResult)->
+        {
 
+            player.sendMessage(Text.literal("hello"), false);
+            BlockState targetedBlock = world.getBlockState(hitResult.getBlockPos());
+            if (targetedBlock.getBlock() instanceof WallSignBlock)
+            {
+                player.sendMessage(Text.literal("I saw the sign"), true);
+                player.sendMessage(Text.literal(targetedBlock.get(WallSignBlock.FACING).asString()), false);
+                if (targetedBlock.get(WallSignBlock.FACING) == Direction.EAST)
+                    player.sendMessage(Text.literal("It's facing east"), false);
+                return ActionResult.SUCCESS;
+            }
+            return ActionResult.PASS;
+
+        });
     }
 }
